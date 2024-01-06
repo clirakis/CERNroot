267a171b3dcb82f07d34655093446dc81eaa3271
Chris Lirakis <clirakis@gmail.com>
2024-01-06 11:17:30 -0500

    First cut at making a nautical compass rose using the CERN root framework.

---
+++ Compass/ChartRose.cpp
@@ -0,0 +1,241 @@
+/********************************************************************
+ *
+ * Module Name : ChartRose.cpp
+ *
+ * Author/Date : C.B. Lirakis / 06-Jan-24
+ *
+ * Description : Generic ChartRose
+ *
+ * Restrictions/Limitations :
+ *
+ * Change Descriptions :
+ *
+ * Classification : Unclassified
+ *
+ * References :
+ *
+ ********************************************************************/
+// System includes.
+
+#include <iostream>
+using namespace std;
+#include <string>
+#include <cmath>
+
+// Local Includes.
+#include "ChartRose.hh"
+
+/**
+ ******************************************************************
+ *
+ * Function Name : ChartRose constructor
+ *
+ * Description :
+ *
+ * Inputs :
+ *
+ * Returns :
+ *
+ * Error Conditions :
+ * 
+ * Unit Tested on: 
+ *
+ * Unit Tested by: CBL
+ *
+ *
+ *******************************************************************
+ */
+ChartRose::ChartRose (Int_t csize, Float_t Magnetic) : TObject()
+{
+   // Create a Compass rose in a new canvas.
+
+   fPad = new TCanvas("Compass:canvas","Compass",-csize,csize);
+   fPad->SetFillColor(0);     // white
+   fPad->SetLineColor(6);     // pinkish. 
+   fScale = 1.0/((Float_t) csize);
+   fMagnetic = Magnetic * TMath::DegToRad();
+   SetBit(kCanDelete);
+   Draw();                    // append this Rose to fPad
+}
+
+/**
+ ******************************************************************
+ *
+ * Function Name : ChartRose destructor
+ *
+ * Description :
+ *
+ * Inputs :
+ *
+ * Returns :
+ *
+ * Error Conditions :
+ * 
+ * Unit Tested on: 
+ *
+ * Unit Tested by: CBL
+ *
+ *
+ *******************************************************************
+ */
+ChartRose::~ChartRose (void)
+{
+}
+
+
+/**
+ ******************************************************************
+ *
+ * Function Name : ChartRose function
+ *
+ * Description :
+ *
+ * Inputs :
+ *
+ * Returns :
+ *
+ * Error Conditions :
+ * 
+ * Unit Tested on: 
+ *
+ * Unit Tested by: CBL
+ *
+ *
+ *******************************************************************
+ */
+void ChartRose::Paint(Option_t *)
+{
+    const Float_t kZero        =  0.5;
+    const Float_t kCrossDim    =  5.0;   // These are just an arbitray scale.
+    const Float_t kDiv1        =  5.0;   // Divisions around True and mag 
+    const Float_t kDiv2        =  3.5;
+    const Float_t kDiv3        =  2.0;
+    const Float_t kRadius1     = 83.0;
+    const Float_t kOuterMajorPoints = 92.0;
+    const Float_t kOuterPoints =  8.0;
+
+    static RosePoints *point1 = new RosePoints();
+    static RosePoints *point2 = new RosePoints();
+
+    Float_t x0,y0, x1, y1;      // Working variables. 
+    Float_t Angle, dAngle;
+    Float_t wh = (Float_t)fPad->XtoPixel(fPad->GetX2());
+    Float_t hh = (Float_t)fPad->YtoPixel(fPad->GetY1());
+    Float_t aspect = wh/hh;
+
+    x0 = -kCrossDim*fScale/2.0 + kZero;
+    x1 =  kCrossDim*fScale/2.0 + kZero;
+    y0 = -kCrossDim*fScale/2.0*aspect + kZero;
+    y1 =  kCrossDim*fScale/2.0*aspect + kZero;
+
+    // Start simple make the cross in the middle. 
+    fPad->PaintLine(x0, kZero, x1, kZero);
+    // And rotate 90 degrees. 
+    fPad->PaintLine(kZero, y0, kZero, y1);
+ 
+    // Draw some other static features
+    // 4 longish lines along true north. 
+    Angle = 0.0;
+    for (UInt_t i=0; i<4; i++)
+    {
+	y0 = kOuterMajorPoints * fScale;
+	y1 = y0 + kOuterPoints * fScale;
+	point1->SetXY(0.0, y0);
+	point2->SetXY(0.0, y1);
+
+	point1->Rotate(Angle);
+	point2->Rotate(Angle);
+
+	if (wh < hh) 
+	{                   // scale in oder to draw circle scale
+	    point1->Scale(0.5,0.5*wh/hh);
+	    point2->Scale(0.5,0.5*wh/hh);
+	} 
+	else 
+	{
+	    point1->Scale(0.5*hh/wh,0.5);
+	    point2->Scale(0.5*hh/wh,0.5);
+	}
+
+	point1->Shift(0.5,0.5);              // move to center of pad
+	point2->Shift(0.5,0.5);
+
+	fPad->PaintLine(point1->GetX(),point1->GetY(),point2->GetX(),point2->GetY());
+
+	Angle = Angle + 90.0;
+    }
+
+    Angle = 0.0;
+    // Draw outer rose scale
+    for (UInt_t i=0; i<360; i++)
+    {
+	y0 = kRadius1*fScale;   // 0 degrees, North. 
+	point1->SetXY(0.0, y0);
+	if (i%10==0)
+	{
+	    y1 = y0 + kDiv1*fScale;
+	}
+	else if (i%5==0)
+	{
+	    y1 = y0 + kDiv2*fScale;
+	}
+	else
+	{
+	    y1 = y0 + kDiv3*fScale;
+	}
+	point2->SetXY(0.0, y1);
+
+	point1->Rotate(Angle);
+	point2->Rotate(Angle);
+
+	if (wh < hh) 
+	{                   // scale in oder to draw circle scale
+	    point1->Scale(0.5,0.5*wh/hh);
+	    point2->Scale(0.5,0.5*wh/hh);
+	} 
+	else 
+	{
+	    point1->Scale(0.5*hh/wh,0.5);
+	    point2->Scale(0.5*hh/wh,0.5);
+	}
+
+	point1->Shift(0.5,0.5);              // move to center of pad
+	point2->Shift(0.5,0.5);
+
+	fPad->PaintLine(point1->GetX(),point1->GetY(),point2->GetX(),point2->GetY());
+
+	Angle = Angle + 1.0;
+    }
+   
+#if 0
+
+   Float_t wh = (Float_t)fPad->XtoPixel(fPad->GetX2());
+   Float_t hh = (Float_t)fPad->YtoPixel(fPad->GetY1());
+
+   for (int i = 0; i < 60; i++ ) 
+   {  
+      // draw minute/hour ticks
+      point1->SetXY(0.,0.9);
+
+      if (!(i%5)) point2->SetXY(0.,0.8);       // hour  ticks  are longer
+      else        point2->SetXY(0.,0.87);
+
+      Float_t angle = 6.*i;
+      point1->Rotate(angle);
+      point2->Rotate(angle);
+
+      if (wh < hh) {                   // scale in oder to draw circle scale
+         point1->Scale(0.5,0.5*wh/hh);
+         point2->Scale(0.5,0.5*wh/hh);
+      } else {
+         point1->Scale(0.5*hh/wh,0.5);
+         point2->Scale(0.5*hh/wh,0.5);
+      }
+
+      point1->Shift(0.5,0.5);              // move to center of pad
+      point2->Shift(0.5,0.5);
+
+      fPad->PaintLine(point1->GetX(),point1->GetY(),point2->GetX(),point2->GetY());
+   }
+#endif
+}
---
+++ Compass/ChartRose.hh
@@ -0,0 +1,90 @@
+/**
+ ******************************************************************
+ *
+ * Module Name : ChartRose.hh
+ *
+ * Author/Date : C.B. Lirakis / 06-Jan-24
+ *
+ * Description : Make an image of a modern day 
+ *               compass rose fit for a chart. An amusement.
+ *
+ * Restrictions/Limitations :
+ *
+ * Change Descriptions :
+ *
+ * Classification : Unclassified
+ *
+ * References :
+ * a very old clock demo that Rene Brun did in root. I don't remember
+ * where I found it, but I can not find it again. 
+ *
+ * http://root.cern.ch
+ *
+ *******************************************************************
+ */
+#ifndef __CHARTROSE_hh_
+#define __CHARTROSE_hh_
+#include <TTimer.h>
+#include <TCanvas.h>
+#include <TPolyLine.h>
+#include <TDatime.h>
+#include <TPoints.h>
+#include <TMath.h>
+
+// Outer compass rose
+class RosePoints : public TPoints {
+
+public:
+   RosePoints(Coord_t x=0, Coord_t y=0) : TPoints(x,y) { }
+   ~RosePoints() { }
+
+   void SetXY(Coord_t x, Coord_t y) { SetX(x); SetY(y); }
+
+   void Rotate(Float_t Angle)  // Rotates the coordinate system a angle degrees clockwise
+   {
+
+      Float_t rX, rY;
+      Float_t theta = Angle*TMath::DegToRad();  
+
+      rX = GetX()*TMath::Cos(theta)+GetY()*TMath::Sin(theta);
+      rY = GetY()*TMath::Cos(theta)-GetX()*TMath::Sin(theta);
+      SetXY(rX,rY);
+   }
+
+   void Scale(Float_t factorX, Float_t factorY) { SetX(GetX()*factorX); SetY(GetY()*factorY); }
+   void Shift(Coord_t x, Coord_t y) { SetX(GetX()+x); SetY(GetY()+y); }
+};
+
+
+/// ChartRose documentation here. 
+class ChartRose : public TObject {
+public:
+    /// Default Constructor 
+    /// default magnetic offset to true north is 4.0 degrees.  
+    ChartRose( Int_t csize=100, Float_t Magnetic=4.0);
+    /// Default destructor
+    virtual ~ChartRose();
+
+    /*!
+     * Description: 
+     *   
+     *
+     * Arguments:
+     *   
+     *
+     * Returns:
+     *
+     * Errors:
+     *
+     */
+   void   Paint(Option_t *option);
+
+   ClassDef(ChartRose,0)  // Chart based compass rose
+
+private:
+    TPad       *fPad;            // pad where this compass rose is drawn
+    Float_t   fScale;
+    Float_t   fMagnetic;        // Angular offset of magnetic north
+
+};
+#endif
